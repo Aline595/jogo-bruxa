eac94816ce2d0ece6adc7b5af91933b66081670a
Aline <aline.as385>
2020-06-29 17:05:10 -0300

    commit final

--- sketch.js
+++ sketch.js
@@ -1,27 +1,22 @@
-
-let imagemCenario;
-let imagemPersonagem;
-let cenario;
-let somDojogo;
-let personagem;
-
-function preload(){
-  imagemCenario = loadImage('imagens/cenario/floresta.png');
-  imagemPersonagem = loadImage('imagens/personagem/correndo.png');
-  somDojogo = loadSound('sons/trilha_jogo.mp3')
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  cenario = new Cenario(imagemCenario, 3);
-  personagem = new Personagem(imagemPersonagem);
   frameRate(40); //controle de frames velocidadedo fundo
   somDojogo.loop();
+  jogo = new Jogo();
+  telaInicial = new TelaInicial();
+  jogo.setup();
+  //cenaAtual = 'jogo';
+  cenas = {
+    jogo,
+    telaInicial
+  };
+  botaoGerenciador = new BotaoGerenciador('Iniciar', width/2, height/2);
+}
+
+function keyPressed(){
+  jogo.keyPressed(key);
 }
 
 function draw() {
-  cenario.exibe();
-  cenario.move();
-  
-  personagem.exibe();
+  cenas[cenaAtual].draw();
 }
